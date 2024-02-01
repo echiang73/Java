5b095bd3106d365afd729e409d8f697e39bcac96
Eddie Chiang <echiang73@gmail.com>
2024-02-01 12:49:32 -0700

    Create BarkingDog.java

---
+++ Udemy Masterclass Coding Exercises/CE4: BarkingDog/BarkingDog.java
@@ -0,0 +1,17 @@
+// Basic solution
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (!barking || hourOfDay < 0 || hourOfDay > 24) {
+             return false;
+        }
+        return (hourOfDay >= 0 && hourOfDay < 8) || hourOfDay == 23;
+     }
+}
+
+
+// Alternative solution using ternary operator
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        return barking && hourOfDay >= 0 && (hourOfDay < 8 || hourOfDay == 23);
+    }
+}
