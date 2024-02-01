0e08127e3cc8c84ba69c574f7a780ed7152d50f5
Eddie Chiang <echiang73@gmail.com>
2024-02-01 12:26:40 -0700

    Create SpeedConverter.java

---
+++ Udemy Masterclass Coding Exercises/SpeedConverter.java
@@ -0,0 +1,32 @@
+public class SpeedConverter {
+    public static long toMilesPerHour (double kilometersPerHour) {
+        long conversion = Math.round(kilometersPerHour / 1.609);
+        if (kilometersPerHour < 0) {
+            return -1;
+        } else {
+            return conversion;
+        }
+    }
+    
+    public static void printConversion (double kilometersPerHour ) {
+        long milesPerHour = toMilesPerHour(kilometersPerHour);
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+    }
+}
+
+// // Alternative solution using ternary operators
+// public class SpeedConverter {
+//     public static long toMilesPerHour (double kilometersPerHour) {
+//         long conversion = Math.round(kilometersPerHour / 1.609);
+//         return kilometersPerHour < 0 ? -1 : conversion;
+//     }
+    
+//     public static void printConversion (double kilometersPerHour ) {
+//         long milesPerHour = toMilesPerHour(kilometersPerHour);
+//         System.out.println(kilometersPerHour < 0 ? "Invalid Value" : (kilometersPerHour + " km/h = " + milesPerHour + " mi/h"));
+//     }
+// }
