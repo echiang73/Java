cc0a61dab4d1750b5a443392c3f6168c6621c6a6
Eddie Chiang <echiang73@gmail.com>
2024-02-04 01:33:26 -0700

    Fix comment

--- Udemy Masterclass Coding Exercises/CE19: Shared Digit/SharedDigit.java
+++ Udemy Masterclass Coding Exercises/CE19: Shared Digit/SharedDigit.java
@@ -1,4 +1,4 @@
-// Basic solution with nested loop, O(n^2) not performant
+// Basic solution with nested loops, O(n^2) not performant
 public class SharedDigit {
     public static boolean hasSharedDigit(int num1, int num2) {
         if (num1 < 10 || num1 > 99 || num2 < 10 || num2 > 99) {
