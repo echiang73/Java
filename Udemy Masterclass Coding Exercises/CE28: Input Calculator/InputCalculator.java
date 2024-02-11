8fcc56006d32303dfacdc426f7e93bd300ae84d5
Eddie Chiang <echiang73@gmail.com>
2024-02-10 23:34:29 -0700

    Add alternative solution without casting

--- Udemy Masterclass Coding Exercises/CE28: Input Calculator/InputCalculator.java
+++ Udemy Masterclass Coding Exercises/CE28: Input Calculator/InputCalculator.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class InputCalculator {
     public static void inputThenPrintSumAndAverage() {
         Scanner input = new Scanner(System.in);
-        int sum=0;
+        int sum = 0;
         long avg = 0;
-        int count=0;
+        int count = 0;
 
         while (true) {
             String str = input.nextLine();
@@ -40,6 +40,27 @@ public class InputCalculator {
             count++;
         }
         avg = Math.round((double)sum/count);
+        System.out.println("SUM = " + sum + " AVG = " + avg);
+        input.close();
+    }
+}
+
+
+// Alternative solution using hasNextInt() and no casting
+import java.util.Scanner;
+
+public class InputCalculator {
+    public static void inputThenPrintSumAndAverage() {
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+        long avg = 0;
+        double count = 0;
+
+        while (input.hasNextInt()) {
+            sum += input.nextInt();
+            count++;
+        }
+        avg = Math.round(sum/count);
         System.out.println("SUM = " + sum + " AVG = " + avg);
         input.close();
     }
