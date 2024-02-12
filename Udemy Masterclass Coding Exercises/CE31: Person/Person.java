60ac2405378d04b8b3bcc12bdd1c72f506bea0a8
Eddie Chiang <echiang73@gmail.com>
2024-02-11 21:20:12 -0700

    Upload initial files

---
+++ Udemy Masterclass Coding Exercises/CE31: Person/Person.java
@@ -0,0 +1,48 @@
+// Basic solution
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    
+    public String getFirstName() {
+        return firstName;
+    }
+ 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+ 
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+ 
+    public void setAge(int age) {
+        this.age = (age < 0 || age > 100) ? 0 : age;
+    }   
+     
+    public boolean isTeen() {
+        return age > 12 && age < 20;
+    }
+    
+    public String getFullName(){
+        if (firstName.isEmpty() && !lastName.isEmpty()) {
+            return lastName;
+        } else if (!firstName.isEmpty() && lastName.isEmpty()) {
+            return firstName;
+        } else if (firstName.isEmpty() && lastName.isEmpty()) {
+            return "";
+        }
+        return firstName + " " + lastName;
+    }
+}
+
+
+
