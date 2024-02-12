9cdd96709b995e2cfc4b1eaf1a367d2f33fc4b3e
Eddie Chiang <echiang73@gmail.com>
2024-02-11 21:03:46 -0700

    Add alternative solution with best practices

--- Udemy Masterclass Coding Exercises/CE29: Paint Job/PaintJob.java
+++ Udemy Masterclass Coding Exercises/CE29: Paint Job/PaintJob.java
@@ -23,3 +23,22 @@ public class PaintJob {
         return (int) Math.ceil(area / areaPerBucket);
     }
 }
+
+
+// Alternative solution following best practices to call method from method
+public class PaintJob {
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
+        if(width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0){
+            return -1;
+        }
+        return (int) Math.ceil((width * height / areaPerBucket) - extraBuckets);
+    }
+    
+    public static int getBucketCount(double width, double height, double areaPerBucket){
+        return getBucketCount(width, height, areaPerBucket, 0); // call first method
+    }
+    
+    public static int getBucketCount(double area, double areaPerBucket){
+        return getBucketCount(area, 1, areaPerBucket, 0); // call first method
+    }
+}
