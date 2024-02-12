a43cffa757eb1306c72eda4ec1462f14d671ef0f
Eddie Chiang <echiang73@gmail.com>
2024-02-11 21:35:33 -0700

    Format document

--- Udemy Masterclass Coding Exercises/CE29: Paint Job/PaintJob.java
+++ Udemy Masterclass Coding Exercises/CE29: Paint Job/PaintJob.java
@@ -1,6 +1,6 @@
 // Basic solution
 public class PaintJob {
-    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
         if(width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0){
             return -1;
         }
@@ -8,7 +8,7 @@ public class PaintJob {
         return (int) Math.ceil((area / areaPerBucket) - extraBuckets);
     }
     
-    public static int getBucketCount(double width, double height, double areaPerBucket){
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
         if(width <= 0 || height <= 0 || areaPerBucket <= 0){
             return -1;
         }
@@ -16,7 +16,7 @@ public class PaintJob {
         return (int) Math.ceil(area / areaPerBucket);
     }
     
-    public static int getBucketCount(double area, double areaPerBucket){
+    public static int getBucketCount(double area, double areaPerBucket) {
         if(area <= 0 || areaPerBucket <= 0){
             return -1;
         }
@@ -27,18 +27,18 @@ public class PaintJob {
 
 // Alternative solution following best practices to call method from method
 public class PaintJob {
-    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
         if(width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0){
             return -1;
         }
         return (int) Math.ceil((width * height / areaPerBucket) - extraBuckets);
     }
     
-    public static int getBucketCount(double width, double height, double areaPerBucket){
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
         return getBucketCount(width, height, areaPerBucket, 0); // call first method
     }
     
-    public static int getBucketCount(double area, double areaPerBucket){
+    public static int getBucketCount(double area, double areaPerBucket) {
         return getBucketCount(area, 1, areaPerBucket, 0); // call first method
     }
 }
