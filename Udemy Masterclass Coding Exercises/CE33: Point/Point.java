f9f05511cf71d8f1dd5bbba6dd157f74a1999a5d
Eddie Chiang <echiang73@gmail.com>
2024-02-12 23:51:51 -0700

    Add alternative solution with overloaded method best practices

--- Udemy Masterclass Coding Exercises/CE33: Point/Point.java
+++ Udemy Masterclass Coding Exercises/CE33: Point/Point.java
@@ -85,3 +85,47 @@ public class Point {
         return Math.sqrt(Math.pow(this.x-x, 2) + Math.pow(this.y-y, 2));
     }
 }
+
+
+// Alternative solution using best practices for overloaded methods
+public class Point {
+    private int x;
+    private int y;
+ 
+    public Point() {
+        this(0, 0);
+    }
+ 
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+ 
+    public int getX() {
+        return this.x;
+    }
+ 
+    public int getY() {
+        return this.y;
+    }
+ 
+    public void setX(int x) {
+        this.x = x;
+    }
+ 
+    public void setY(int y) {
+        this.y = y;
+    }
+ 
+    public double distance() {
+        return distance(0, 0);
+    }
+ 
+    public double distance(Point point) {
+        return distance(point.x, point.y);
+    }
+ 
+    public double distance(int x, int y) {
+        return Math.sqrt(Math.pow(this.x-x, 2) + Math.pow(this.y-y, 2));
+    }
+}
