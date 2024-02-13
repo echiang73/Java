79a8219e92a0412958c58d49d2b3934acf77bb6a
Eddie Chiang <echiang73@gmail.com>
2024-02-13 12:51:25 -0700

    Add alternative solution using ternary operators

--- Udemy Masterclass Coding Exercises/CE34: Carpet Cost Calculator/Floor.java
+++ Udemy Masterclass Coding Exercises/CE34: Carpet Cost Calculator/Floor.java
@@ -22,3 +22,18 @@ public class Floor {
     }
 }
 
+
+// Alternative solution using ternary operator
+public class Floor {
+    private double width;
+    private double length;
+ 
+    public Floor(double width, double length) {
+        this.width = width < 0 ? 0 : width;
+        this.length = length <  0 ? 0 : length;
+    }
+    
+    public double getArea() {
+        return width*length;
+    }
+}
