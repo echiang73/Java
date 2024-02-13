dce471bcbd06ad2e27a39c78e1001abc6628d836
Eddie Chiang <echiang73@gmail.com>
2024-02-12 23:44:53 -0700

    Add alternative solution with constructor calling setters

--- Udemy Masterclass Coding Exercises/CE32: Wall Area/Wall.java
+++ Udemy Masterclass Coding Exercises/CE32: Wall Area/Wall.java
@@ -83,3 +83,38 @@ public class Wall {
         return width * height;
     }
 }
+
+
+// Alternative solution with constructor calling setters (not necessarily recommended)
+public class Wall {
+    private double width;
+    private double height;
+    
+    public Wall() {
+    }
+    
+    public Wall(double width, double height) {
+        setWidth(width);
+        setHeight(height);
+    }
+    
+    public double getWidth() {
+        return width;
+    }
+    
+    public void setWidth(double width) {
+        this.width = width < 0 ? 0 : width;
+    }
+    
+    public double getHeight() {
+        return height;
+    }
+    
+    public void setHeight(double height) {
+        this.height = height < 0 ? 0 : height;
+    }
+    
+    public double getArea() {
+        return width * height;
+    }
+}
