8b1fd561af939d8cf50e25be047c5be8451ad534
Eddie Chiang <echiang73@gmail.com>
2024-02-14 08:57:07 -0700

    Add alternative solution using ternary operators

--- Udemy Masterclass Coding Exercises/CE36: Cylinder/Cylinder.java
+++ Udemy Masterclass Coding Exercises/CE36: Cylinder/Cylinder.java
@@ -21,3 +21,20 @@ public class Cylinder extends Circle {
 }
 
 
+// Alternative solution using ternary operator
+public class Cylinder extends Circle {
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = (height < 0) ? 0 : height;
+    }
+    
+    public double getHeight() {
+        return height;
+    }
+    
+    public double getVolume() {
+        return height*getArea();
+    }
+}
