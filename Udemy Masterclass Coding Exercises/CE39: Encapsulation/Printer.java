64f1d1a50bf44e5a652cc46abf287aa9c1ee650c
Eddie Chiang <echiang73@gmail.com>
2024-02-16 22:54:52 -0700

    Add alternative solution using ternary operators

--- Udemy Masterclass Coding Exercises/CE39: Encapsulation/Printer.java
+++ Udemy Masterclass Coding Exercises/CE39: Encapsulation/Printer.java
@@ -36,3 +36,31 @@ public class Printer {
         return pagesPrinted;
     }
 }
+
+
+// Alternative solution using ternary operators
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        this.tonerLevel = tonerLevel < 0 || tonerLevel > 100 ? -1 : tonerLevel;
+        this.duplex = duplex;
+        this.pagesPrinted = 0;
+    }
+    
+    public int addToner(int tonerAmount) {
+        return tonerAmount <= 0 || tonerAmount > 100 || tonerLevel + tonerAmount > 100 ? -1 : (tonerLevel += tonerAmount);
+    }
+ 
+    public int printPages(int pages) {
+        int pagesToPrint = (duplex) ? pages/2 + pages%2 : pages;
+        pagesPrinted += pagesToPrint;
+        return pagesToPrint;
+    }
+ 
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+}
