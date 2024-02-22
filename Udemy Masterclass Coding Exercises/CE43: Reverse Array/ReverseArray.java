77c974d7fce3359be80c9c7f6a1d6dc61b73a398
Eddie Chiang <echiang73@gmail.com>
2024-02-21 23:21:40 -0700

    Add alternative solutions using copyOf and clone

--- Udemy Masterclass Coding Exercises/CE43: Reverse Array/ReverseArray.java
+++ Udemy Masterclass Coding Exercises/CE43: Reverse Array/ReverseArray.java
@@ -13,3 +13,33 @@ public class ReverseArray {
         System.out.println("Reversed array = " + Arrays.toString(arr));
     }
 }
+
+
+// Alternative solution using copyOf, not performant by doubling memory
+import java.util.Arrays;
+
+public class ReverseArray {
+    private static void reverse(int[] arr) {
+        System.out.println("Array = " + Arrays.toString(arr));
+        int[] copyArr = Arrays.copyOf(arr, arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = copyArr[arr.length-1 - i];
+        }
+        System.out.println("Reversed array = " + Arrays.toString(arr));
+    }
+}
+
+
+// Alternative solution using clone, not performant by doubling memory
+import java.util.Arrays;
+
+public class ReverseArray {
+    private static void reverse(int[] arr) {
+        System.out.println("Array = " + Arrays.toString(arr));
+        int[] cloneArr = arr.clone();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = cloneArr[arr.length-1 - i];
+        }
+        System.out.println("Reversed array = " + Arrays.toString(arr));
+    }
+}
