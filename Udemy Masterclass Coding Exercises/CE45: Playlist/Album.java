f17a86c0a9a59036980e295dbee0db6d63860d78
Eddie Chiang <echiang73@gmail.com>
2024-02-23 21:41:15 -0700

    Add alternative solutions using String.format()

--- Udemy Masterclass Coding Exercises/CE45: Playlist/Album.java
+++ Udemy Masterclass Coding Exercises/CE45: Playlist/Album.java
@@ -17,3 +17,25 @@ public class Song {
         return title + ": " + duration;
     }
 }
+
+
+// Alternative solution using String.format
+public class Song {
+ 
+    private String title;
+    private double duration;
+ 
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+ 
+    public String getTitle() {
+        return title;
+    }
+ 
+    @Override
+    public String toString() {
+        return String.format("%s: %.1f", title, duration);
+    }
+}
