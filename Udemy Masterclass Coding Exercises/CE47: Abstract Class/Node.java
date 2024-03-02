03053134fe65f59f6b5bd35287e1167a12eef635
Eddie Chiang <echiang73@gmail.com>
2024-03-01 22:43:25 -0700

    Add alternative compareTo() methods

--- Udemy Masterclass Coding Exercises/CE47: Abstract Class/Node.java
+++ Udemy Masterclass Coding Exercises/CE47: Abstract Class/Node.java
@@ -26,14 +26,29 @@ public class Node extends ListItem {
  
     @Override
     int compareTo(ListItem other) {
-        int x = Integer.parseInt(String.valueOf(value)); // parse and return int
-        // int x = Integer.parseInt("" + value); // alternative: concatenate, parse and return int
-        // int x = Integer.valueOf("" + value); // alternative: concatenate and return Integer
+        int x = Integer.parseInt(String.valueOf(value));
         int y = Integer.parseInt(String.valueOf(other.value));
         if (x == y) {
             return 0;
         }
         return x > y ? 1 : -1;
-        // return x == y ? 0 : (x > y ? 1 : -1); // nested ternary operator
     }
 }
+
+
+// Alternative compareTo() method using ternary operator and by concatenating, parse and return int
+    @Override
+    int compareTo(ListItem other) {
+        int x = Integer.parseInt("" + value);
+        int y = Integer.parseInt("" + other.value);
+        return x == y ? 0 : (x > y ? 1 : -1);
+    }
+
+
+// Alternative compareTo() method by concatenating, and use Integer.valueOf() to return Integer
+    @Override
+    int compareTo(ListItem other) {
+        int x = Integer.valueOf("" + value);
+        int y = Integer.valueOf("" + other.value);
+        return x == y ? 0 : (x > y ? 1 : -1);
+    }
