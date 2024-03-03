706b603295cafa6d5b31a7aefa3c0325c9418401
Eddie Chiang <echiang73@gmail.com>
2024-03-02 23:56:26 -0700

    Upload initial files

--- Udemy Masterclass Coding Exercises/CE47: Abstract Class/MyLinkedList.java
+++ Udemy Masterclass Coding Exercises/CE47: Abstract Class/MyLinkedList.java
@@ -1,3 +1,97 @@
+// Basic solution
 public class MyLinkedList implements NodeList {
+    private ListItem root;
+ 
+    public MyLinkedList(ListItem root) {
+        this.root = root;
+    }
+    
+    @Override
+    public ListItem getRoot() {
+        return root;
+    }
+ 
+    @Override
+    public boolean addItem(ListItem node) {
+        if (root == null || node == null) {
+            root = node;
+            return true;
+        }
 
+        int nodeValue = Integer.parseInt("" + node.getValue());
+        ListItem current = root;
+        while (true) {
+            int currentValue = Integer.parseInt("" + current.getValue());
+            if (currentValue == nodeValue) {
+                return false;
+            }
+            if (nodeValue < currentValue) {
+                ListItem oldRoot = root;
+                root = node;
+                root.setNext(oldRoot);
+                oldRoot.setPrevious(root);
+                return true;
+            }
+            else if (nodeValue > currentValue) {
+                if (current.next() == null) {
+                    current.setNext(node);
+                    node.setPrevious(current);
+                    return true;
+                }
+                int nextValue = Integer.parseInt("" + current.next().getValue());
+                if (nextValue > nodeValue) {
+                    node.setNext(current.next());
+                    node.setPrevious(current);
+                    current.next().setPrevious(node);
+                    current.setNext(node);
+                    return true;
+                }
+                current = current.next();
+                continue;
+            }
+        }
+    }
+ 
+    @Override
+    public boolean removeItem(ListItem node) {
+        if (root == null || node == null) {
+            return false;
+        }
+        int nodeValue = Integer.parseInt("" + node.getValue());
+        ListItem current = root;
+        while (true) {
+            int currentValue = Integer.parseInt("" + current.getValue());
+            if (currentValue == nodeValue) {
+                if (current == root) {
+                    root = (current.next() == null) ? null : current.next();
+                    return true;
+                } else if (current.next() == null) {
+                    current.previous().setNext(null);
+                    return true;
+                }
+                current.previous().setNext(current.next());
+                current.next().setPrevious(current.previous());
+                return true;
+            } else if (nodeValue < currentValue) {
+                return false;
+            }
+            current = current.next();
+            if (current == null) {
+                return false;
+            }
+        }
+    }
+ 
+    @Override
+    public void traverse(ListItem root) {
+        if (root == null) {
+            System.out.print("The list is empty");
+            return;
+        }
+        ListItem current = root;
+        while (current != null) {
+            System.out.println(current.getValue());
+            current = current.next();
+        }
+    }
 }
