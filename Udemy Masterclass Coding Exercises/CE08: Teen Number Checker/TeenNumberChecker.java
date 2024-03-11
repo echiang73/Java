df3109cddb98b5c2c935927d1acba05c7e99fb55
Eddie Chiang <echiang73@gmail.com>
2024-03-11 09:46:57 -0700

    Add alternative solution with Arrays.asList()

--- Udemy Masterclass Coding Exercises/CE08: Teen Number Checker/TeenNumberChecker.java
+++ Udemy Masterclass Coding Exercises/CE08: Teen Number Checker/TeenNumberChecker.java
@@ -29,7 +29,21 @@ public class TeenNumberChecker {
 }
 
 
-// Alternative solution using stream and lambda (extra method won't pass Udemy checker)
+// Alternative solution using Arrays.asList(), stream and lambda (extra method won't pass checker):
+import java.util.Arrays;
+ 
+public class TeenNumberChecker {
+    public static boolean hasTeen(int num1, int num2, int num3) {
+        return Arrays.asList(num1, num2, num3).stream().anyMatch(num -> isTeen(num));
+    }
+    
+    public static boolean isTeen(int num) {        
+        return num >= 13 && num <= 19;
+    }
+}
+
+
+// Alternative solution using efficient JDK9 factory method List.of(), stream and lambda (extra method won't pass checker):
 import java.util.List;
  
 public class TeenNumberChecker {
