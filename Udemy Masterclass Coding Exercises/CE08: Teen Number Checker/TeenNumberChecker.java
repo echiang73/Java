eef59a056540bc99fefbc8851b6565522a5fcd47
Eddie Chiang <echiang73@gmail.com>
2024-03-11 09:07:56 -0700

    Add alternative solution using reference method to pass checker

--- Udemy Masterclass Coding Exercises/CE08: Teen Number Checker/TeenNumberChecker.java
+++ Udemy Masterclass Coding Exercises/CE08: Teen Number Checker/TeenNumberChecker.java
@@ -41,3 +41,17 @@ public class TeenNumberChecker {
         return num >= 13 && num <= 19;
     }
 }
+
+
+// Alternative solution using stream and use of reference method to pass (will pass checker):
+import java.util.List;
+
+public class TeenNumberChecker {
+    public static boolean hasTeen(int num1, int num2, int num3) {
+        return List.of(num1, num2, num3).stream().anyMatch(TeenNumberChecker::isTeen);
+    }
+    
+    public static boolean isTeen(int num) {        
+        return num >= 13 && num <= 19;
+    }
+}
