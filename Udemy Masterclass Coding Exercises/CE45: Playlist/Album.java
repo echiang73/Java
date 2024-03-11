fd31c8dc107229197e920f2c2cbf907e172ba954
Eddie Chiang <echiang73@gmail.com>
2024-03-10 23:56:05 -0700

    Add alternative solution using enhanced for loop

--- Udemy Masterclass Coding Exercises/CE45: Playlist/Album.java
+++ Udemy Masterclass Coding Exercises/CE45: Playlist/Album.java
@@ -47,3 +47,54 @@ public class Album {
         return null;
     }
 }
+
+
+// Alternative solution using enhanced for loop
+import java.util.ArrayList;
+import java.util.LinkedList;
+ 
+public class Album {
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+ 
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        songs = new ArrayList<>();
+    }
+ 
+    public boolean addSong(String title, double duration ) {
+        if (findSong(title) == null) {
+            songs.add(new Song(title, duration));
+            return true;
+        }
+        return false;
+    }
+ 
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playlist) {
+         if (trackNumber >= 1) {
+            playlist.add(songs.get(trackNumber - 1));
+            return true;
+        }
+        return false;
+    }
+ 
+    public boolean addToPlayList(String title, LinkedList<Song> playlist) {
+        Song song = findSong(title);
+        if (song == null) {
+            return false;
+        }
+        playlist.add(song);
+        return true;
+    }
+ 
+    private Song findSong(String title) {
+        for (Song song : songs) {
+            if (song.getTitle().equals(title)) {
+                return song;
+            }
+        }
+        return null;
+    }
+}
