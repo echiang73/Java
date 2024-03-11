aaf4f0b0960912c9f06a9bb344c85a3ce1bfe435
Eddie Chiang <echiang73@gmail.com>
2024-03-10 23:55:05 -0700

    Replace Album class

--- Udemy Masterclass Coding Exercises/CE45: Playlist/Album.java
+++ Udemy Masterclass Coding Exercises/CE45: Playlist/Album.java
@@ -1,41 +1,49 @@
 // Basic solution
-public class Song {
-    private String title;
-    private double duration;
+import java.util.ArrayList;
+import java.util.LinkedList;
  
-    public Song(String title, double duration) {
-        this.title = title;
-        this.duration = duration;
+public class Album {
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+ 
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        songs = new ArrayList<>();
     }
  
-    public String getTitle() {
-        return title;
+    public boolean addSong(String title, double duration ) {
+        if (findSong(title) == null) {
+            songs.add(new Song(title, duration));
+            return true;
+        }
+        return false;
     }
  
-    @Override
-    public String toString() {
-        return title + ": " + duration;
-    }
-}
-
-
-// Alternative solution using String.format
-public class Song {
- 
-    private String title;
-    private double duration;
- 
-    public Song(String title, double duration) {
-        this.title = title;
-        this.duration = duration;
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playlist) {
+         if (trackNumber >= 1) {
+            playlist.add(songs.get(trackNumber - 1));
+            return true;
+        }
+        return false;
     }
  
-    public String getTitle() {
-        return title;
+    public boolean addToPlayList(String title, LinkedList<Song> playlist) {
+        Song song = findSong(title);
+        if (song == null) {
+            return false;
+        }
+        playlist.add(song);
+        return true;
     }
  
-    @Override
-    public String toString() {
-        return String.format("%s: %.1f", title, duration);
+    private Song findSong(String title) {
+        for (int i = 0; i < songs.size(); i++) {
+            if (songs.get(i).getTitle().equals(title)) {
+                return songs.get(i);
+            }
+        }
+        return null;
     }
 }
