55861cda80174867818456b73b52b9581cc8b759
Eddie Chiang <echiang73@gmail.com>
2024-03-14 23:38:09 -0700

    Add alternative solution using BigDecimal

--- Udemy Masterclass Coding Exercises/CE06: DecimalComparator/DecimalComparator.java
+++ Udemy Masterclass Coding Exercises/CE06: DecimalComparator/DecimalComparator.java
@@ -17,3 +17,25 @@ public class DecimalComparator {
         return (int) (num1*1000) == (int) (num2*1000);
     }
 }
+
+
+// Alternative solution using Math.abs()
+public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
+        return Math.abs(num1*1000 - num2*1000) < 1;
+    }
+}
+
+
+// Alternative solution using BigDecimal
+import java.math.BigDecimal;
+
+public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
+        BigDecimal bd1 = new BigDecimal("" + num1);
+        BigDecimal bd2 = new BigDecimal("" + num2);
+        double rd1 = bd1.setScale(3, BigDecimal.ROUND_DOWN).doubleValue();
+        double rd2 = bd2.setScale(3, BigDecimal.ROUND_DOWN).doubleValue();
+        return rd1 == rd2;
+    }
+}
