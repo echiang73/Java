2a0b918d46885421a14243703bcd19a97385081e
Eddie Chiang <echiang73@gmail.com>
2024-04-05 11:34:17 -0700

    Replace all solution with correct coding exercise

--- Udemy Masterclass Coding Exercises/CE14: Number Of Days In Month/NumberOfDaysInMonth.java
+++ Udemy Masterclass Coding Exercises/CE14: Number Of Days In Month/NumberOfDaysInMonth.java
@@ -1,145 +1,142 @@
 // Basic solution using traditional switch case
-public class NumberInWord {
-    public static void printNumberInWord(int number){
-        switch(number){
-            case 0:
-                System.out.println("ZERO");
-                break;
-            case 1:
-                System.out.println("ONE");
-                break;
-            case 2:
-                System.out.println("TWO");
-                break;
-            case 3:
-                System.out.println("THREE");
-                break;
-            case 4:
-                System.out.println("FOUR");
-                break;
-            case 5:
-                System.out.println("FIVE");
-                break;
-            case 6:
-                System.out.println("SIX");
-                break;
-            case 7:
-                System.out.println("SEVEN");
-                break;
-            case 8:
-                System.out.println("EIGHT");
-                break;
-            case 9:
-                System.out.println("NINE");
-                break;
-            default:
-                System.out.println("OTHER");
+public class NumberOfDaysInMonth {
+    public static boolean isLeapYear(int year) {
+        if (year >= 1 && year <= 9999) {
+            return year%4 == 0 && year%100 != 0 || year%400 == 0;
         }
+        return false;
+    }
+ 
+    public static int getDaysInMonth(int month, int year) {
+        if (year >= 1 && year <= 9999 && month >= 1 && month <= 12) {
+            switch (month) {
+                case 2: 
+                    return isLeapYear(year) ? 29 : 28;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    return 30;
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    return 31;
+                default:
+                    return -1;
+            }
+        }
+        return -1;
     }
 }
 
 
-// Alternative solution using traditional switch case with cached variable and break
-public class NumberInWord {
-    public static void printNumberInWord(int number){
-        String word;
-        switch(number){
-            case 0:
-                word = "ZERO";
-                break;
-            case 1:
-                word = "ONE";
-                break;
-            case 2:
-                word = "TWO";
-                break;
-            case 3:
-                word = "THREE";
-                break;
-            case 4:
-                word = "FOUR";
-                break;
-            case 5:
-                word = "FIVE";
-                break;
-            case 6:
-                word = "SIX";
-                break;
-            case 7:
-                word = "SEVEN";
-                break;
-            case 8:
-                word = "EIGHT";
-                break;
-            case 9:
-                word = "NINE";
-                break;
-            default:
-                word = "OTHER";
-                break;
+// Alternative solution for conciseness, using default to return 31
+public class NumberOfDaysInMonth {
+    public static boolean isLeapYear(int year) {
+        return year >= 1 && year <= 9999 && (year%4 == 0 && year%100 != 0 || year%400 == 0);
+    }
+ 
+    public static int getDaysInMonth(int month, int year) {
+        if (year >= 1 && year <= 9999 && month >= 1 && month <= 12) {
+            switch (month) {
+                case 2: 
+                    return isLeapYear(year) ? 29 : 28;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    return 30;
+                default:
+                    return 31;
+            }
         }
-        System.out.println(word);
+        return -1;
     }
 }
 
 
-// Alternative solution using enhanced switch case with yield
-public class NumberInWord {
-    public static void printNumberInWord(int number) {
-        String word = switch (number) {
-            case 0: yield "ZERO";
-            case 1: yield "ONE";
-            case 2: yield "TWO";
-            case 3: yield "THREE";
-            case 4: yield "FOUR";
-            case 5: yield "FIVE";
-            case 6: yield "SIX";
-            case 7: yield "SEVEN";
-            case 8: yield "EIGHT";
-            case 9: yield "NINE";
-            default: yield "OTHER";
-        };
-        System.out.println(word);
+// Alternative solution for using enhanced switch with yield
+public class NumberOfDaysInMonth {
+    public static boolean isLeapYear(int year) {
+        return year >= 1 && year <= 9999 && (year%4 == 0 && year%100 != 0 || year%400 == 0);
+    }
+ 
+    public static int getDaysInMonth(int month, int year) {
+        if (year >= 1 && year <= 9999 && month >= 1 && month <= 12) {
+            return switch (month) {
+                case 2: 
+                    yield isLeapYear(year) ? 29 : 28;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    yield 30;
+                default:
+                    yield 31;
+            };
+        }
+        return -1;
+    }
+}
+
+
+// Alternative solution for using enhanced switch with commas in multiple cases
+public class NumberOfDaysInMonth {
+    public static boolean isLeapYear(int year) {
+        return year >= 1 && year <= 9999 && (year%4 == 0 && year%100 != 0 || year%400 == 0);
+    }
+ 
+    public static int getDaysInMonth(int month, int year) {
+        if (year >= 1 && year <= 9999 && month >= 1 && month <= 12) {
+            return switch (month) {
+                case 2: 
+                    yield isLeapYear(year) ? 29 : 28;
+                case 4, 6, 9, 11:
+                    yield 30;
+                default:
+                    yield 31;
+            };
+        }
+        return -1;
     }
 }
 
 
 // Alternative solution using enhanced switch case with arrow function
-public class NumberInWord {
-    public static void printNumberInWord(int number) {
-        String word = switch (number) {
-            case 0 -> "ZERO";
-            case 1 -> "ONE";
-            case 2 -> "TWO";
-            case 3 -> "THREE";
-            case 4 -> "FOUR";
-            case 5 -> "FIVE";
-            case 6 -> "SIX";
-            case 7 -> "SEVEN";
-            case 8 -> "EIGHT";
-            case 9 -> "NINE";
-            default -> "OTHER";
-        };
-        System.out.println(word);
+public class NumberOfDaysInMonth {
+    public static boolean isLeapYear(int year) {
+        return year >= 1 && year <= 9999 && (year%4 == 0 && year%100 != 0 || year%400 == 0);
+    }
+ 
+    public static int getDaysInMonth(int month, int year) {
+        if (year >= 1 && year <= 9999 && month >= 1 && month <= 12) {
+            return switch (month) {
+                case 2 -> isLeapYear(year) ? 29 : 28;
+                case 4, 6, 9, 11 -> 30;
+                default -> 31;
+            };
+        }
+        return -1;
     }
 }
 
 
-// Alternative solution using nested enhanced switch case with arrow function inside println()
-public class NumberInWord {
-    public static void printNumberInWord(int number) {
-        System.out.println(switch (number) {
-            case 0 -> "ZERO";
-            case 1 -> "ONE";
-            case 2 -> "TWO";
-            case 3 -> "THREE";
-            case 4 -> "FOUR";
-            case 5 -> "FIVE";
-            case 6 -> "SIX";
-            case 7 -> "SEVEN";
-            case 8 -> "EIGHT";
-            case 9 -> "NINE";
-            default -> "OTHER";
-        });
+// Alternative solution using ternary operator with nested enhanced switch case
+public class NumberOfDaysInMonth {
+    public static boolean isLeapYear(int year) {
+        return year >= 1 && year <= 9999 && (year%4 == 0 && year%100 != 0 || year%400 == 0);
     }
-}  
+ 
+    public static int getDaysInMonth(int month, int year) {
+        return (year < 1 || year > 9999 || month < 1 || month > 12) ? -1 :
+            switch (month) {
+                case 2 -> isLeapYear(year) ? 29 : 28;
+                case 4, 6, 9, 11 -> 30;
+                default -> 31;
+            };
+    }
+}
