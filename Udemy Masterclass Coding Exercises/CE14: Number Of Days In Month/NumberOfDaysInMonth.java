ea60990120814537188e72d18dc6ff0f00951399
Eddie Chiang <echiang73@gmail.com>
2024-04-07 01:27:10 -0700

    Add alternative solution using YearMonth

--- Udemy Masterclass Coding Exercises/CE14: Number Of Days In Month/NumberOfDaysInMonth.java
+++ Udemy Masterclass Coding Exercises/CE14: Number Of Days In Month/NumberOfDaysInMonth.java
@@ -197,3 +197,20 @@ public class NumberOfDaysInMonth {
         return cal.getActualMaximum(Calendar.DAY_OF_MONTH);
     }
 }
+
+
+// Alternative solution using imported YearMonth
+import java.time.YearMonth;
+
+public class NumberOfDaysInMonth {
+    public static boolean isLeapYear(int year){
+        return year >= 1 && year <= 9999 && (year%4 == 0 && year%100 != 0 || year%400 == 0);
+    }
+    
+    public static int getDaysInMonth(int month, int year) {
+        if (month < 1 || month > 12 || year < 1 || year > 9999) {
+            return -1;
+        }
+        return YearMonth.of(year, month).lengthOfMonth();
+    }
+}
