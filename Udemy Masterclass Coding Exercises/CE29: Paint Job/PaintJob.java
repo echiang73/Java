05cefb215f441b762db131e2ffeaf0ef4ec3f808
Eddie Chiang <echiang73@gmail.com>
2024-04-11 23:45:46 -0700

    Add alternative solution having fewest argument execute main calculation

--- Udemy Masterclass Coding Exercises/CE29: Paint Job/PaintJob.java
+++ Udemy Masterclass Coding Exercises/CE29: Paint Job/PaintJob.java
@@ -61,3 +61,28 @@ public class PaintJob {
         return getBucketCount(area, 1, areaPerBucket); // call second method
     }
 }
+
+
+// Alternative solution having fewest argument execute main calculation
+public class PaintJob {
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
+        if(width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0){
+            return -1;
+        }
+        return getBucketCount(width * height, areaPerBucket) - extraBuckets; // call third method
+    }
+    
+    public static int getBucketCount(double width, double height, double areaPerBucket){
+        if(width <= 0 || height <= 0 || areaPerBucket <= 0){
+            return -1;
+        }
+        return getBucketCount(width * height, areaPerBucket); // call third method
+    }
+    
+    public static int getBucketCount(double area, double areaPerBucket){
+        if(area <= 0 || areaPerBucket <= 0){
+            return -1;
+        }
+        return (int) Math.ceil(area / areaPerBucket);
+    }
+}
