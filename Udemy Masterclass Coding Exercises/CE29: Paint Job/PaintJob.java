04ffba5e485359e8f2c9f0790a697cdccdcb981d
Eddie Chiang <echiang73@gmail.com>
2024-04-11 23:41:50 -0700

    Add alternative solution using chaining

--- Udemy Masterclass Coding Exercises/CE29: Paint Job/PaintJob.java
+++ Udemy Masterclass Coding Exercises/CE29: Paint Job/PaintJob.java
@@ -42,3 +42,22 @@ public class PaintJob {
         return getBucketCount(area, 1, areaPerBucket, 0); // call first method
     }
 }
+
+
+// Alternative solution following best practices to call method from method, like constructor chaining
+public class PaintJob {
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
+        if(width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0){
+            return -1;
+        }
+        return (int) Math.ceil((width * height / areaPerBucket) - extraBuckets);
+    }
+    
+    public static int getBucketCount(double width, double height, double areaPerBucket){
+        return getBucketCount(width, height, areaPerBucket, 0); // call first method
+    }
+    
+    public static int getBucketCount(double area, double areaPerBucket){
+        return getBucketCount(area, 1, areaPerBucket); // call second method
+    }
+}
