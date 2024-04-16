5cb037cb00c64daf7dc6c5e89cae861912631a67
Eddie Chiang <echiang73@gmail.com>
2024-04-16 17:11:47 +0000

    Add user prompt

--- Poker/Poker.java
+++ Poker/Poker.java
@@ -5,9 +5,34 @@ import java.util.*;
 public class Poker {
     Scanner scanner = new Scanner(System.in);
     Random random = new Random();
+    String playerInput;
 
     public void startGame() {
         System.out.println("\nWelcome to the Java Poker Table");
+
+        playerInput = promptUserInput();
+
+        while(true) {
+            if ("P".equals(playerInput)) {
+                // pokerGame();
+                playerInput = promptUserInput();
+            } else if ("Q".equals(playerInput)) {
+                // printWinPercentageMessage();
+                System.out.println("Thanks for playing Java Poker, goodbye!");
+                scanner.close();
+                break;
+            } else {
+                System.out.println("Invalid entry, enter P or Q");
+                playerInput = scanner.nextLine().toUpperCase();
+            }
+
+        }
+    }
+
+    private String promptUserInput() {
+        System.out.println("Please select an option.");
+        System.out.println("P -> Play\nQ -> Quit");
+        return scanner.nextLine().toUpperCase();
     }
 
 }
