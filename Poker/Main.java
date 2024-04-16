9001e29a8cbef3c76f474fc7ac99a274ffb9dca0
Eddie Chiang <echiang73@gmail.com>
2024-04-16 09:52:27 -0700

    Upload initial files

--- Poker/Main.java
+++ Poker/Main.java
@@ -2,7 +2,8 @@ package Poker;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Poker poker = new Poker();
+        poker.startGame();
     }
 
 }
---
+++ Poker/Poker.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Poker {
+    Scanner scanner = new Scanner(System.in);
+    Random random = new Random();
+
+    public void startGame() {
+        System.out.println("\nWelcome to the Java Poker Table");
+    }
+
+}
