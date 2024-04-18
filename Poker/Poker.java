b7e240ba06d8b5129ea85dcb17fcf63a4c5196c9
Eddie Chiang <echiang73@gmail.com>
2024-04-18 14:45:09 +0000

    Add win percentage logic

--- Poker/Poker.java
+++ Poker/Poker.java
@@ -6,6 +6,7 @@ public class Poker {
     Scanner scanner = new Scanner(System.in);
     Random random = new Random();
     String playerInput;
+    double winPercent = 0;
 
     public void startGame() {
         System.out.println("\nWelcome to the Java Poker Table");
@@ -17,7 +18,7 @@ public class Poker {
                 // pokerGame();
                 playerInput = promptUserInput();
             } else if ("Q".equals(playerInput)) {
-                // printWinPercentageMessage();
+                printWinPercentageMessage();
                 System.out.println("Thanks for playing Java Poker, goodbye!");
                 scanner.close();
                 break;
@@ -33,6 +34,16 @@ public class Poker {
         System.out.println("Please select an option.");
         System.out.println("P -> Play\nQ -> Quit");
         return scanner.nextLine().toUpperCase();
+    }
+
+    private void printWinPercentageMessage() {
+        if (winPercent > 70) {
+            System.out.println("You are a professional hustler!");
+        } else if (winPercent > 50) {
+            System.out.println("Good idea to leave while you are ahead.");
+        } else {
+            System.out.println("Blackjack might not be your cup of tea.  Try slots.");
+        }
     }
 
 }
